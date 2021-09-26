f4fcec503bf1f0386618a2bbb81e8140a543e847
Ezequiel Oliveira <contato.oliveiraezequiel@gmail.com>
2021-09-26 16:25:20 -0300

    atualização do menu mobile

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,14 @@
 
 // Menu
 let btn = document.querySelector("#btn")
+let btnMenu = document.querySelector("#btn-menu")
 let sidebar = document.querySelector(".sidebar")
 let searchBtn = document.querySelector(".bx-search")
 
 btn.onclick = function() {
+    sidebar.classList.toggle("active")
+}
+btnMenu.onclick = function() {
     sidebar.classList.toggle("active")
 }
 searchBtn.onclick = function() {
